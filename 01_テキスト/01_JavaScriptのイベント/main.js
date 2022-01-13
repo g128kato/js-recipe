720557cfb5d75f2be417a8c5a405ab6076b4577b
g128kato <g520128u@mails.cc.ehime-u.ac.jp>
2022-01-13 16:36:23 +0900

    [3の倍数]を学習しました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("button")
+
+const alertMessage = function () {
+  alert("クリックしたね")
+}
+
+button.onclick = alertMessage
+
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+document.onkeydown = function (e) {
+  console.log(e.key)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
