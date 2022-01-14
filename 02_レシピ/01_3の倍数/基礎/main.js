9ca3e214db57400571e618f74eda791f92b8939d
g128kato <g520128u@mails.cc.ehime-u.ac.jp>
2022-01-14 11:07:56 +0900

    [3の倍数]を学習しました。

--- 02_レシピ/01_3の倍数/基礎/main.js
+++ 02_レシピ/01_3の倍数/基礎/main.js
@@ -1,7 +1,7 @@
 const genkiFunction = function (number) {
   for (let n = 1; n <= number; n++) {
     if (n % 3 === 0) {
-      console.log(n + "!!!!!!!")
+      console.log(`${n}!!!!!!!`)
     } else {
       console.log(n)
     }
